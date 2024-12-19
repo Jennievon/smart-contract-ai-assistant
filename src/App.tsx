01090e7208d9390f6e7e51e82695abb148e2af64
Jennifer Echenim <echenimjennifer@gmail.com>
2024-12-19 15:15:26 +0400

    🎉 initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="text-4xl font-bold">Hello, Vite + React!</h1>
+        <p
+          className="text-lg text-
+        gray-500"
+        >
+          Edit src/App.tsx and save to reload.
+        </p>
+        <a
+          className="text-blue-600"
+          href="https://vitejs.dev/guide/features.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vite Docs
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
